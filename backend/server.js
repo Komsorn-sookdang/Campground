e696841d929683ffc3dd8e15238986ae2d006b26
มึน <komsornzaza@gmail.com>
2023-04-22 03:04:32 +0700

    Add Cors policy

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const auth = require("./routes/auth");
 const expressValidator = require("express-validator");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -13,6 +14,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors());
 // app.use(expressValidator);
 app.use(cookieParser());
 
