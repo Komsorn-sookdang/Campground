50a33152ad82880da9e1bae95a91174aab0b9ba2
kharit <qwertyunity123@gmail.com>
2023-04-28 18:37:33 +0700

    add io

--- backend/controllers/appointment.js
+++ backend/controllers/appointment.js
@@ -47,6 +47,13 @@ exports.getAppointment = async (req,res,next)=>{
             return res.status(404).json({success:false, message: `No appointment with the id of ${req.params.id}`});
         }
 
+        if (appointment.user.toString()!== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({
+                success:false,
+                message: `User ${req.user.id} is not authorized to view this appointment`
+            });
+        }
+
         return res.status(200).json({
             success:true,
             data: appointment
