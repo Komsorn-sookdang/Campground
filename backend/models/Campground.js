b6565b15c2a5db05a08889c1ff91e30241da468e
มึน <komsornzaza@gmail.com>
2023-05-06 13:11:53 +0700

    WIP

--- backend/models/Campground.js
+++ backend/models/Campground.js
@@ -1,36 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const CampgroundSchema = new mongoose.Schema({
+const CampgroundSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true,'Please add a name'],
-        unique: true,
-        trim: true,
-        maxlength: [50,'Name can not be more than 50 characters']
+      type: String,
+      required: [true, "Please add a name"],
+      unique: true,
+      trim: true,
+      maxlength: [50, "Name can not be more than 50 characters"],
     },
     address: {
-        type: String,
-        required: [true,'Please add a address']
+      type: String,
+      required: [true, "Please add a address"],
     },
     tel: {
-        type: String,
+      type: String,
     },
-}, {
-    toJSON: {virtuals: true},
-    toObject: {virtuals: true}
-});
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 // Cascade delete bookings when a Campground is deleted
-CampgroundSchema.pre('remove', async function(next) {
-    console.log(`Bookings being removed from Campground ${this._id}`);
-    await this.model('Booking').deleteMany({Campground: this._id});
-    next();
+CampgroundSchema.pre("remove", async function (next) {
+  console.log(`Bookings being removed from Campground ${this._id}`);
+  await this.model("Booking").deleteMany({ Campground: this._id });
+  next();
 });
 // Reverse populate with virtuals
-CampgroundSchema.virtual('bookings', {
-    ref: 'Booking',
-    localField: '_id',
-    foreignField: 'campground',
-    justOne: false
+CampgroundSchema.virtual("bookings", {
+  ref: "Booking",
+  localField: "_id",
+  foreignField: "campground",
+  justOne: false,
 });
 
-module.exports = mongoose.model('Campground', CampgroundSchema);
+module.exports = mongoose.model("Campground", CampgroundSchema);
